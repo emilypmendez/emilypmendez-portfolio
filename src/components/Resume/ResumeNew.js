403f2967be7cad9b4557b474496b9dbf0a6aae88
Emily Mendez <emilypmendez@gmail.com>
2022-03-27 14:44:19 -0400

    Updated resume PDF file

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,7 +3,7 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Experience from "../Resume/Experience";
 // import Particle from "../Particle";
-import pdf from "../../assets/EPM_Resume.pdf";
+import pdf from "../../assets/EPM-Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
